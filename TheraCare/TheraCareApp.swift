28ab9c040766365d5d43ef359f9e35a63081d1bc
Muhammed <olayinkafacus@gmail.com>
2023-08-01 13:38:28 -0400

    Initial Commit

---
+++ TheraCare/TheraCareApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheraCareApp.swift
+//  TheraCare
+//
+//  Created by Yinka  Facus  on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct TheraCareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
